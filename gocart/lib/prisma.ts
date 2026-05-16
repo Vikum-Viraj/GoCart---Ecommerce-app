d8510f526f884fe4d707e838dac627a31545ceef
Vikum Manchanayake <vikumviraj@vizuamatix.com>
2026-05-16 08:46:54 +0530

    initial commit

---
+++ gocart/lib/prisma.ts
@@ -0,0 +1,17 @@
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { neonConfig } from "@neondatabase/serverless";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter: new PrismaNeon({ connectionString: process.env.DATABASE_URL! }),
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
